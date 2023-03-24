d8ff8391fb702ad8f6f48366342cc819515e4d0d
kenkwanatgai <kjjkjjzyayufqza@yahoo.com>
2023-03-24 23:10:08 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import * as ort from 'onnxruntime-web';
-import model from "./data.onnx?url";
+
 
 function App() {
   const [count, setCount] = useState(0)
-
-  async function run() {
-    // create a new session and load the AlexNet model.
-    const session = await ort.InferenceSession.create(model);
-    // prepare inputs. a tensor need its corresponding TypedArray as data
-    const dataA = Float32Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-    const dataB = Float32Array.from([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]);
-    const tensorA = new ort.Tensor('float32', dataA, [3, 4]);
-    const tensorB = new ort.Tensor('float32', dataB, [4, 3]);
-
-    // prepare feeds. use model input names as keys.
-    const feeds = { a: tensorA, b: tensorB };
-
-    // feed inputs and run
-    const results = await session.run(feeds);
-
-    // read from results
-    const dataC = results.c.data;
-    document.write(`data of result tensor 'c': ${dataC}`);
-  }
-
-  useEffect(() => {
-    run();
-  }, [])
 
   return (
     <div className="App">
